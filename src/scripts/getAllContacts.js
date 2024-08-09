87f25498e40a8d9777cf235152198531a24bb5be
Volodymyr Stelmakh <volodymyr.kai@gmail.com>
2024-08-09 20:02:45 +0300

    commit

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,5 +1,20 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
+import { isValidJSON } from '../utils/isValidJSON.js';
 
-export const getAllContacts = async () => {};
+export const getAllContacts = async () => {
+  try {
+    const dataJSON = await fs.readFile(PATH_DB, 'utf8');
+    if (!isValidJSON(dataJSON)) {
+      throw new Error('Файл містить невалідний JSON');
+    }
+    const data = Array.isArray(JSON.parse(dataJSON))
+      ? JSON.parse(dataJSON)
+      : [];
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-console.log(await getAllContacts());
+ console.log(await getAllContacts());
